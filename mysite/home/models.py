001e68f81af878728e87930b92124ec82166e76d
kalumba Samuel <kalumbasml@gmail.com>
2023-06-15 09:51:47 +0300

    nm

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -4,4 +4,18 @@ from django.db import models
 class PContactUs(models.Model):
     name = models.CharField(max_length=30)
     email = models.EmailField()
+    content = models.CharField(max_length=200)
     date_written = models.DateField()
+
+    def __str__():
+        return self.name
+
+class AdminPost(models.Model):
+    pub_date = models.DateField()
+    headline = models.CharField(max_length=200)
+    content = models.TextField()
+    Poster = models.ForeignKey(Poster, on_delete=models.CASCADE)
+    public = models.Boolean()
+
+    def __str__(self):
+        return self.headline
