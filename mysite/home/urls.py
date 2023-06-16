669cfde2afe8e209295f9679d0b4b74dc06b4826
Samuel Kalumba <kalumbasml@gmail.com>
2023-06-16 12:56:08 +0000

    nm

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import  views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('contactus/', views.contactus, name='contactus'),
+    path('login/', views.login, name='login'),
 ]
--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+def home(request):
+    return render(request, "home/home.html", {})
+
 def contactus(request):
     return render(request, "home/contactus.html", {})
 
 def register(request):
     return render(request, "home/register.html", {})
+
+def login(request):
+    return render(request, "home/login.html", {})
