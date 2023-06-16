e82edc589155691e4f347a87dcc8ccad4f3f9905
Samuel Kalumba <kalumbasml@gmail.com>
2023-06-16 10:47:33 +0000

    contact us & register & base

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, "home/index.html", {})
+def contactus(request):
+    return render(request, "home/contactus.html", {})
+
+def register(request):
+    return render(request, "home/register.html", {})
